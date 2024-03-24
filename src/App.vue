c15475d2427dea4881f9a8c27ef6ec7217475b93
zls5600 <2712035095@qq.com>
2024-03-25 02:22:24 +0800

    使用vuex定义好全局信息

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    <BasicLayout />
+  </div>
+</template>
+
+<style>
+#app {
+}
+</style>
+<script setup lang="ts">
+import BasicLayout from "@/layouts/BasicLayout.vue";
+</script>
---
+++ src/store/user.ts
@@ -0,0 +1,21 @@
+import { StoreOptions } from "vuex";
+
+export default {
+  namespaced: true,
+  state: () => ({
+    loginUser: {
+      userName: "未登录",
+    },
+  }),
+  actions: {
+    getLoginUser({ commit, state }, payload) {
+      // todo 从远程获取请求信息
+      commit("updateUser", { userName: "zackzhang" });
+    },
+  },
+  mutations: {
+    updateUser(state, payload) {
+      state.loginUser = payload;
+    },
+  },
+} as StoreOptions<any>;
