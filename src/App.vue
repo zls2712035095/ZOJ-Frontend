efc993f47b50656004f3e04bbc3e633ce9f701b1
zls5600 <2712035095@qq.com>
2024-03-26 23:57:52 +0800

    vue中预留一个可以编写全局初始化逻辑的代码

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,21 @@
 import BasicLayout from "@/layouts/BasicLayout.vue";
 import { useRouter } from "vue-router";
 import { useStore } from "vuex";
+import { onMounted } from "vue";
+
+/**
+ * 全局初始化函数，有全局单次调用的代码，都在此处
+ */
+const doInit = () => {
+  console.log("hello 欢迎来到ZOJ");
+};
+onMounted(() => {
+  doInit();
+});
 
 const router = useRouter();
 const store = useStore();
+
 router.beforeEach((to, from, next) => {
   //仅管理员可见，判断当前用户是否有权限
   if (to.meta?.access === "canAdmin") {
