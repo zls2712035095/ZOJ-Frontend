1266a546bbdba3a4f1ab71e73a66220da9231112
zls5600 <2712035095@qq.com>
2024-03-27 23:36:20 +0800

    生成了前端代码，并更改了从远程获取请求信息

--- src/store/user.ts
+++ src/store/user.ts
@@ -1,18 +1,26 @@
 import { StoreOptions } from "vuex";
 import ACCESS_ENUM from "@/access/ACCESS_ENUM";
+import { UserControllerService } from "../../generated";
 
 export default {
   namespaced: true,
   state: () => ({
     loginUser: {
       userName: "未登录",
-      userRole: ACCESS_ENUM.NOT_LOGIN,
     },
   }),
   actions: {
-    getLoginUser({ commit, state }, payload) {
-      // todo 从远程获取请求信息
-      commit("updateUser", payload);
+    async getLoginUser({ commit, state }, payload) {
+      // 从远程获取请求信息
+      const res = await UserControllerService.getLoginUserUsingGet();
+      if (res.code === 0) {
+        commit("updateUser", res.data);
+      } else {
+        commit("updateUser", {
+          ...state.loginUser,
+          userRole: ACCESS_ENUM.NOT_LOGIN,
+        });
+      }
     },
   },
   mutations: {
