c38d3dce994f31569c7fed11322ed1f30594f6b5
zackzhang <zackzhang@futu.com>
2024-04-02 21:32:36 +0800

    添加了markdow，并且可以正常显示

---
+++ src/components/MdEditor.vue
@@ -0,0 +1,52 @@
+<template>
+  <Editor :value="value" :plugins="plugins" @change="handleChange" />
+</template>
+
+<script setup lang="ts">
+import gfm from "@bytemd/plugin-gfm";
+import { Editor, Viewer } from "@bytemd/vue-next";
+import { ref, withDefaults, defineProps } from "vue";
+import breaks from "@bytemd/plugin-breaks";
+import frontmatter from "@bytemd/plugin-frontmatter";
+import gemoji from "@bytemd/plugin-gemoji";
+import highlight from "@bytemd/plugin-highlight";
+import highlightSsr from "@bytemd/plugin-highlight-ssr";
+import math from "@bytemd/plugin-math";
+import mathSsr from "@bytemd/plugin-math-ssr";
+import mediumZoom from "@bytemd/plugin-medium-zoom";
+import mermaid from "@bytemd/plugin-mermaid";
+
+interface Props {
+  value: string;
+  handleChange: (v: string) => void;
+}
+
+const plugins = [
+  gfm(),
+  // Add more plugins here
+  breaks(),
+  frontmatter(),
+  gemoji(),
+  highlight(),
+  highlightSsr(),
+  math(),
+  mathSsr(),
+  mediumZoom(),
+  mermaid(),
+];
+
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+  handleChange: (v: string) => {
+    console.log(v);
+  },
+});
+const value = ref("");
+
+const handleChange = (v: string) => {
+  value.value = v;
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
--- src/views/NoAuthView.vue
+++ src/views/NoAuthView.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="home">无管理员权限</div>
+  <MdEditor />
 </template>
 
-<script lang="ts"></script>
+<script setup lang="ts">
+import { defineComponent } from "vue";
+import MdEditor from "@/components/MdEditor.vue";
+</script>
