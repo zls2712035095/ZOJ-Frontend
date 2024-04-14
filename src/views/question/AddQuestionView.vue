c45779168e84c41b006285f7c5ed8c9d91777f94
zls5600 <2712035095@qq.com>
2024-04-14 21:29:49 +0800

    更改题目限制样式

--- src/views/question/AddQuestionView.vue
+++ src/views/question/AddQuestionView.vue
@@ -15,7 +15,11 @@
     </a-form-item>
     <a-form-item label="题目限制" :content-flex="false" :merge-props="false">
       <a-space direction="vertical">
-        <a-form-item field="judgeConfig.timeLimit" label="时间限制">
+        <a-form-item
+          field="judgeConfig.timeLimit"
+          label="时间限制"
+          style="min-width: 480px"
+        >
           <a-input-number
             v-model="form.judgeConfig.timeLimit"
             placeholder="请输入时间限制"
@@ -24,7 +28,11 @@
           />
         </a-form-item>
 
-        <a-form-item field="judgeConfig.memoryLimit" label="内存限制">
+        <a-form-item
+          field="judgeConfig.memoryLimit"
+          label="内存限制"
+          style="min-width: 480px"
+        >
           <a-input-number
             v-model="form.judgeConfig.memoryLimit"
             placeholder="请输入内存限制"
@@ -33,7 +41,11 @@
           />
         </a-form-item>
 
-        <a-form-item field="judgeConfig.stackLimit" label="堆栈限制">
+        <a-form-item
+          field="judgeConfig.stackLimit"
+          label="堆栈限制"
+          style="min-width: 480px"
+        >
           <a-input-number
             v-model="form.judgeConfig.stackLimit"
             placeholder="请输入堆栈限制"
